ec0bb77c54431cdd7f3d8bb9fc515826ac246344
luije87 <luije87@gmail.com>
2016-01-19 11:06:47 -0500

    Changes in my new branch

--- MVCExample/Controllers/AccountController.cs
+++ MVCExample/Controllers/AccountController.cs
@@ -20,7 +20,7 @@ namespace MVCExample.Controllers
 
         public AccountController()
         {
-
+            string branch;
         }
 
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
